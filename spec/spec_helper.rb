d9371822ed6414c15563592c71c934421c40b04f
Oswaldo Ferreira <oswluizf@gmail.com>
2016-02-14 19:20:45 -0200

    Cleans spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,8 @@
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../../spec/dummy/config/environment.rb",  __FILE__)
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
-require "rails/all"
-require 'pry'
-require 'rspec/rails'
-
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+require "action_controller"
+require "rspec/rails"
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
